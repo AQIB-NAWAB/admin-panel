4fd0b2e5614ff591a0ac00f900b62ae1fd0e279f
AQIB-NAWAB <aqibnawab1100@gmail.com>
2025-06-23 12:49:57 +0500

    reoslve the url conflict

--- backend/dist/config/typeorm.config.js
+++ backend/dist/config/typeorm.config.js
@@ -3,13 +3,12 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.typeOrmConfig = void 0;
 const typeOrmConfig = (configService) => ({
     type: 'postgres',
-    host: configService.get('DB_HOST'),
-    port: configService.get('DB_PORT'),
-    username: configService.get('DB_USERNAME'),
-    password: configService.get('DB_PASSWORD'),
-    database: configService.get('DB_NAME'),
+    url: configService.get('DATABASE_URL'),
     autoLoadEntities: true,
     synchronize: configService.get('ENV') !== 'production',
+    ssl: {
+        rejectUnauthorized: false,
+    },
 });
 exports.typeOrmConfig = typeOrmConfig;
 //# sourceMappingURL=typeorm.config.js.map
--- backend/src/config/typeorm.config.ts
+++ backend/src/config/typeorm.config.ts
@@ -1,17 +1,11 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { ConfigService } from '@nestjs/config';
 
-// Configuration to connect PostgreSQL database using TypeORM
-
 export const typeOrmConfig = (
   configService: ConfigService,
 ): TypeOrmModuleOptions => ({
   type: 'postgres',
-  host: configService.get<string>('DB_HOST'),
-  port: configService.get<number>('DB_PORT'),
-  username: configService.get<string>('DB_USERNAME'),
-  password: configService.get<string>('DB_PASSWORD'),
-  database: configService.get<string>('DB_NAME'),
+  url: configService.get<string>('DATABASE_URL'),
   autoLoadEntities: true,
   synchronize: configService.get<string>('ENV') !== 'production',
   ssl: {
