8be0ee8f457785946526a799c8a5db133eb540e4
AQIB-NAWAB <aqibnawab1100@gmail.com>
2025-06-23 12:55:34 +0500

    revert

--- backend/src/config/typeorm.config.ts
+++ backend/src/config/typeorm.config.ts
@@ -1,14 +1,17 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { ConfigService } from '@nestjs/config';
 
+// Configuration to connect PostgreSQL database using TypeORM
+
 export const typeOrmConfig = (
   configService: ConfigService,
 ): TypeOrmModuleOptions => ({
   type: 'postgres',
-  url: configService.get<string>('DATABASE_URL'),
+  host: configService.get<string>('DB_HOST'),
+  port: configService.get<number>('DB_PORT'),
+  username: configService.get<string>('DB_USERNAME'),
+  password: configService.get<string>('DB_PASSWORD'),
+  database: configService.get<string>('DB_NAME'),
   autoLoadEntities: true,
   synchronize: configService.get<string>('ENV') !== 'production',
-  ssl: {
-    rejectUnauthorized: false,
-  },
 });
